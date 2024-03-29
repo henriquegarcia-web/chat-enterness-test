efd3eca45458c5fb05e466b75818a2571b6836c7
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-03-29 20:45:56 -0300

    fix: Correções para subir servidor IV

--- server/src/server.js
+++ server/src/server.js
@@ -30,8 +30,8 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
-    dialectModule: 'mysql2',
     logging: false
   }
 )
