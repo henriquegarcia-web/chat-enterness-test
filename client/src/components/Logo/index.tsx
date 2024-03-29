0b4ca5ad6393ec6de40fd560e90aa49c9d2126ff
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-03-28 23:28:22 -0300

    chore: Modificando as propriedades do componente Logo

--- client/src/components/Logo/index.tsx
+++ client/src/components/Logo/index.tsx
@@ -1,24 +1,23 @@
 import { PiChatsCircleLight } from 'react-icons/pi'
 
-import { TypeText } from '@/@types/fonts'
-
 interface ILogo {
-  size?: TypeText
-  iconSize?: TypeText
+  minify?: boolean
 }
 
-const Logo = ({ size, iconSize }: ILogo) => {
-  const logoSizeClasses = size
-    ? `text-${size} font-light`
-    : 'text-4xl font-light'
-  const iconSizeClasses = iconSize ? `text-${iconSize}` : 'text-5xl'
+const Logo = ({ minify = false }: ILogo) => {
+  const logoSizeClasses = minify ? 'text-xl' : 'text-4xl'
+  const iconSizeClasses = minify ? 'text-3xl' : 'text-5xl'
 
   return (
-    <h1 className={`flex items-center gap-2 ${logoSizeClasses}`}>
-      Realtime
-      <PiChatsCircleLight className={iconSizeClasses} />
-      <b className="font-medium">Chat</b>
-    </h1>
+    <div className="flex items-center gap-2">
+      <PiChatsCircleLight
+        className={`font-light leading-6 ${iconSizeClasses}`}
+      />
+      <h1 className={logoSizeClasses}>
+        Realtime
+        <b className="font-medium">Chat</b>
+      </h1>
+    </div>
   )
 }
 
